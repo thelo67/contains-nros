bb423cf8f594c8d758e125d36a499c820b6f2e73
Helo Tuomo <Tuomo.Helo@turkuamk.fi>
2020-03-30 09:20:38 +0300

    Add operation displayResult

--- js/contains_number.js
+++ js/contains_number.js
@@ -8,3 +8,16 @@ const containsNumber = function(numbers, aNumber) {
   return false;
 
 }
+
+const displayResult = function() {
+
+  let oneNumber = 11;
+  let someNumbers = [1,4,2,5,6,3,5,3,2];
+
+  if (containsNumber(someNumbers, oneNumber)) {
+      return `Array contains the number ${oneNumber}`;
+  } else {
+      return `Array doesn't contain the number ${oneNumber}`;
+  }
+
+}
